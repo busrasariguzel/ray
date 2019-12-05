d6e4d286c166f818d3d0589312c1e83315ce8389
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-12-05 15:30:52 -0500

    functions added

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 const Ray = () => {
-  
+  return {
+    
+    length : 0,
+
+
+
+
+    
+    push : function(str){
+      this[this.length] = str;
+      this.length++;
+    },
+
+    pop : function(){
+      const toBeRemoved = this[this.length-1]
+      delete this[this.length-1]
+      this.length--;
+
+      return toBeRemoved;
+    },
+
+    unshift : function(str){
+      for (let i = this.length; i>0; i--) {
+      this[i] = this[i-1]
+      }
+      this[0] = str;
+      this.length++;
+    },
+  shift : function(){
+
+      
+    }
+
+
+
+
+  }
+
+
 }
 
 module.exports = {
